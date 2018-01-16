574fa647a5db4659c0cacdbbc470ef175ba03614
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-16 10:13:34 +0530

    added three test cases

--- task1.js
+++ task1.js
@@ -2,4 +2,7 @@ function upperCaser(input)
 {
   return input.toUpperCase();
 }
-module.exports = upperCaser
+//module.exports = upperCaser
+console.log("to convert a string with only alphabets to uppercase\t",upperCaser("this is a test case")=="THIS IS A TEST CASE");
+console.log("to covert a string to upper case with numbers\t",upperCaser("this is with numbers 1234")=="THIS IS WITH NUMBERS 1234");
+console.log("to covert a string to upper case with special characters\t",upperCaser("this is with numbers ,.;")=="THIS IS WITH NUMBERS ,.;");

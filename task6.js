7086616d42246fbf105acc8c86149d4a860b8eb3
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-17 11:14:32 +0530

    added proper naming convention

--- task6.js
+++ task6.js
@@ -2,7 +2,7 @@ function countWords(inputWords) {
   // SOLUTION GOES HERE
   return inputWords.reduce(function(count, word) {
     if(count[word]){
-      count[word]=++count[word];
+      count[word]+=1;
     }
     else {
       count[word]=1;

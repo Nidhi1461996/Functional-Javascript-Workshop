2e3bcce230b74310cc8eecd2864f520ad2be5e4f
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-17 13:05:21 +0530

    finished 7th task

---
+++ task7.js
@@ -0,0 +1,21 @@
+
+function reduce(arr, fn, initial) {
+  if(arr.length===0){
+    return initial;
+  }
+  let num2=arr[0];
+  initial = fn(num2,initial);
+  arr=arr.slice(1);
+  return reduce(arr,fn,initial);
+
+}
+
+function fn(num1,num2){
+  //console.log(num1,num2,"......");
+  return num1+num2;
+}
+//base case
+console.log("Base Case testing\t", reduce([1,2,3,4,5],fn,0)===15);
+
+//empty array
+console.log("testing for empty array\t", reduce([],fn,0)===0);

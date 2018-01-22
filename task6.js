fe9342dfa1a626197f86ce6d54f37dd8e6c340a2
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-22 22:49:32 +0530

    added few more test cases

--- task6.js
+++ task6.js
@@ -13,6 +13,9 @@ function countWords(inputWords) {
 }
 
 
-console.log("Checking for the base case\t",countWords(["Apple", "Banana", "Apple", "Durian", "Durian", "Durian"]).toString()==={ Apple: 2, Banana: 1, Durian: 3 }.toString());
-console.log("Checking for empty list\t",countWords([]).toString()==={}.toString());
+console.log("Checking for the base case\t",JSON.stringify(countWords(["Apple", "Banana", "Apple", "Durian", "Durian", "Durian"]))===JSON.stringify({ Apple: 2, Banana: 1, Durian: 3 }));
+console.log("Checking for empty list\t",JSON.stringify(countWords([]))===JSON.stringify({}));
+console.log("Checking for list with empty string\t",JSON.stringify(countWords(["Apple", "Banana", "Apple", "", "", "Durian"]))===JSON.stringify({"Apple":2,"Banana":1,"":2,"Durian":1}));
+console.log("Checking for list with only one instance of each string\t",JSON.stringify(countWords(["Banana", "Apple","Durian"]))===JSON.stringify({"Banana":1,"Apple":1,"Durian":1}));
+//module.exports = countWords;
 //module.exports = countWords;

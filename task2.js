7e549fc462441f4447598bf8cb2e6d3452ef5bbf
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-22 22:26:36 +0530

    added one more test case

--- task2.js
+++ task2.js
@@ -2,7 +2,7 @@ let count=0;
 function repeat(operation, num)
 {
   // SOLUTION GOES HERE
-  if(num<=0)
+  if(num<=0 || num===undefined )
   {
     return count;
   }
@@ -18,13 +18,14 @@ function  operation()
 }
 
 //for positive number
-console.log("checking for repeat function\t", repeat(operation,4)===4);
+console.log("checking for repeat function for base case\t", repeat(operation,4)===4);
 count=0;
 //for negative number
-console.log("checking for repeat function\t", repeat(operation,-4)===0);
+console.log("checking for repeat function with negative number\t", repeat(operation,-4)===0);
 count=0;
 //for 0
-console.log("checking for repeat function\t", repeat(operation,0)===0);
+console.log("checking for repeat function with the second argument as 0\t", repeat(operation,0)===0);
 
+console.log("without passing the second parameter\t", repeat(operation)===0);
 // Do not remove the line below
 //module.exports = repeat

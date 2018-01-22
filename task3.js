f937b9bb13eb2666392d5a0874ccdc60d7e17a20
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-22 22:25:57 +0530

    added one more test case

--- task3.js
+++ task3.js
@@ -4,8 +4,10 @@ function doubleAll(numbers) {
   });
 }
 
-console.log("with positive numbers", doubleAll([1,2,3,4]).toString()===[2,4,6,8].toString());
-console.log("with positive numbers", doubleAll([-1,-2,3,4]).toString()===[-2,-4,6,8].toString());
-console.log("with positive numbers", doubleAll([]).toString()===[].toString());
+console.log("with positive numbers\t", doubleAll([1,2,3,4]).toString()===[2,4,6,8].toString());
+console.log("with positive numbers\t", doubleAll([-1,-2,3,4]).toString()===[-2,-4,6,8].toString());
+console.log("with positive numbers\t", doubleAll([]).toString()===[].toString());
+console.log("validation of false condition\t", doubleAll([1,2]).toString()!==[1,2].toString());
+
 
 //module.exports = doubleAll

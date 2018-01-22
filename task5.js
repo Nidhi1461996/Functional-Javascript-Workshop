89bae8f0af2259c90ae5889b8bdb4a338c082672
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-22 22:33:16 +0530

    added two more test cases

--- task5.js
+++ task5.js
@@ -16,6 +16,26 @@ var goodUsers = [
   { id: 3 }
 ];
 
+var users=[
+  { id: 1 },
+  { id: 2}
+];
+
+//basecase Testing
+
+console.log("Users are good users\t", checkUsersValid(goodUsers)(users)===true);
+
+users=[];
+console.log("With empty users\t", checkUsersValid(goodUsers)(users)===true);
+
+var users=[
+  { id: 1 },
+  { id: 4}
+];
+
+console.log("With empty users\t", checkUsersValid(goodUsers)(users)===false);
+
+
 
 
 

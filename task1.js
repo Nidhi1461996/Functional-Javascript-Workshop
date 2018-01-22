8f80caeb97baf3de6f5a791ca8e058f6124b2ba4
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-01-22 22:27:32 +0530

    added a test case with empty string

--- task1.js
+++ task1.js
@@ -6,3 +6,4 @@ function upperCaser(input)
 console.log("to convert a string with only alphabets to uppercase\t",upperCaser("this is a test case")==="THIS IS A TEST CASE");
 console.log("to covert a string to upper case with numbers\t",upperCaser("this is with numbers 1234")==="THIS IS WITH NUMBERS 1234");
 console.log("to covert a string to upper case with special characters\t",upperCaser("this is with numbers ,.;")==="THIS IS WITH NUMBERS ,.;");
+console.log("to check with empty string\t",upperCaser("")==="");
